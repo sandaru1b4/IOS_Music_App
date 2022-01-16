af99c9207ac86c582d9d4ed96b7fbc3329acfca9
sandaru1b4 <83119823+sandaru1b4@users.noreply.github.com>
2022-01-16 14:13:14 +0530

    Tab Bar added

--- Canciones/ViewController/MainVC.swift
+++ Canciones/ViewController/MainVC.swift
@@ -1,13 +1,13 @@
 //
-//  FirstVC.swift
+//  MainVC.swift
 //  Canciones
 //
-//  Created by Achitha Sandaruwan on 1/14/22.
+//  Created by Achitha Sandaruwan on 1/16/22.
 //
 
 import UIKit
 
-class FirstVC: UIViewController {
+class MainVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
