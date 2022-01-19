c96a9c42de07143894b2310a0e3da7152958acd1
sandaru1b4 <83119823+sandaru1b4@users.noreply.github.com>
2022-01-20 01:48:05 +0530

    page control indicator setup, go to Home.

--- Canciones/ViewController/OnboardingVC.swift
+++ Canciones/ViewController/OnboardingVC.swift
@@ -10,11 +10,25 @@ import UIKit
 class OnboardingVC: UIViewController {
     
     
-    @IBOutlet weak var collectionViw: UICollectionView!
+    @IBOutlet weak var collectionView: UICollectionView!
     @IBOutlet weak var nextBtn: UIButton!
     @IBOutlet weak var pageControl: UIPageControl!
     
     var slides: [OnboardingSlide] = []
+    
+    var currentPage = 0 {
+        /// A property observer
+        didSet {
+            /// When currentPage changes it assigns to pageControll
+            pageControl.currentPage = currentPage
+            /// When currentPage changes Button title changes
+            if currentPage == slides.count - 1 {
+                nextBtn.setTitle("Get Started", for: .normal)
+            } else {
+                nextBtn.setTitle("Next", for: .normal)
+            }
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,8 +42,26 @@ class OnboardingVC: UIViewController {
     
     @IBAction func nextBtnAction(_ sender: Any) {
         
+        if currentPage == slides.count - 1 {
+            /// Redirecting to Main storyboard
+            /// perform the segue
+            performSegue(withIdentifier: "GotoMain", sender: self)
+            
+        } else {
+            currentPage += 1
+            /// Setting an Index path
+            let indexPath = IndexPath(item: currentPage, section: 0)
+            /// Scroll the collection view to specific slide
+            collectionView.scrollToItem(at: indexPath, at: .centeredHorizontally, animated: true)
+        }
     }
     
+    /// Segue happen if the sugue identifier matches
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "GotoMain" {
+            guard segue.destination is UITabBarController else { return }
+        } else {}
+    }
     
 }
 
@@ -48,6 +80,14 @@ extension OnboardingVC: UICollectionViewDelegate, UICollectionViewDataSource, UI
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         return CGSize(width: collectionView.frame.width, height: collectionView.frame.height)
+    }
+    
+    /// find the page in the scroll view
+    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+        /// Getting the width of a scroll view single frame
+        let width = scrollView.frame.width
+        /// Getting the page by deviding full width of the scrooll view
+        currentPage = Int(scrollView.contentOffset.x / width)
     }
     
 }
--- Canciones/ViewController/TabBarController.swift
+++ Canciones/ViewController/TabBarController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class MainVC: UIViewController {
+class TabBarController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
